5ea4c3addc521b3a9c47f4d369e6c23217911d93
Felix Zheng <zheng.felix@outlook.com>
2022-11-25 10:44:50 -0500

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+const app = express();
+const router = express.Router();
+import mysql from 'mysql';
+
+//Assigning Constants
+const port = process.env.PORT || 3000; 
+// const host = 'localhost';
+// const usr = '';
+// const pswd = '';
+// const db = '';
+
+// app.use(express.json());
+
+// //connect to mysql db
+// const connection = mysql.createConnection({
+//   host: `${host}`,
+//   user: `${usr}`,
+//   password: `${pswd}`,
+//   database: `${db}`
+// })
+
+// connection.connect(function(err){
+//     if(err) {throw err}
+//     else {console.log("Connected to MySQL DB")}
+// });
+
+
+/** Connection example:
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+**/
+
+//using express to load front end files from static
+app.use('/',express.static('static')); 
+
+//middleware for logging 
+app.use((req,res,next)=>{
+    console.log(`${req.method} request for ${req.url}`)
+    next();
+});
+
+app.use("/api",router)
+
+app.listen(port,() => { console.log(`listening on port ${port}...`)});
